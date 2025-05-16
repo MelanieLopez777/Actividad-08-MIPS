b50be70f840c6f75f40e6952eebb5be3b98afd95
DGR21 <diegodelamora08@gmail.com>
2025-05-16 14:21:08 -0600

    Arreglo multiplicacion por 4

--- DataPath/shift_left2_tipo_j.v
+++ DataPath/shift_left2_tipo_j.v
@@ -4,7 +4,7 @@ module shift_left2_tipo_j (
 );
     
 always @* begin
-    o_shifted_value_jump = {i_shifted_value_jump << 2, 2'b00};
+    o_shifted_value_jump = {i_shifted_value_jump, 2'b00};
 end
 
 endmodule
