6c1056af934e88fd47eca5bb5219c05d2fbe40a0
DGR21 <diegodelamora08@gmail.com>
2025-05-18 03:04:42 -0600

    Revert "Algoritmo completado"
    This reverts commit 74f674fc6604e2ee1521858767e8c98e83ff4be9.

--- Codigo Ensamblador/Codigo_ensamblador_algoritmo.asm
+++ Codigo Ensamblador/Codigo_ensamblador_algoritmo.asm
@@ -1,28 +1,3 @@
-lw   $2, $1, 0   # Cargo el numero 5381
-lw   $3, $1, 8   # Cargo el numero de chars, en este caso 3 - 1
-addi $1, $1, 14  # Sumar 10 al 4 para poder cargar los caracteres
-nop
-nop
-nop
-lbu  $5, $1, 0   # Cargo la letra en ascci
-sll  $6, $2, 5
-nop
-nop
-nop
-add  $6, $6, $2
-nop
-nop
-nop
-add  $6, $6, $5
-nop
-nop
-nop
-beq  $4, $3, 5
-addi $4, $4, 1
-addi $1, $1, -1
-addi $2, $6, 0
-j   6
-nop
-nop
-nop
-sw $2, $1, 5
+lbu $4, $1, 2
+lbu $3, $1, 1
+lbu $2, $1, 0
